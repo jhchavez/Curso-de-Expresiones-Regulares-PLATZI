50a002bd7e03dac9861b6116837699c6401178c0
Jhon Chavez <jhchg7@gmail.com>
2020-05-24 02:10:01 -0400

    E.R PYTHON

---
+++ python_regex.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import re
+
+pattern = re.compile(r'^([\d]{4,4})\-\d\d-\d\d,(.+),(.+),(\d+),(\d+),.*$')
+
+f = open("./results.csv", "r")
+
+for line in f:
+    res = re.match(pattern, line)
+    if res:
+        total = int(res.group(4)) + int(res.group(5))
+        if total > 10:
+            print "goles: %d, %s %s,%s [%d-%d]" %  \
+            (total, res.group(1), res.group(2), \
+                res.group(3), int(res.group(4)), \
+                int(res.group(5)))
+
+f.close()
