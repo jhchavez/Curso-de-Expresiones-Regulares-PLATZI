794efc5a57fba8617d44d6fd26b058ba36791d22
Jhon Chavez <jhchg7@gmail.com>
2020-05-24 01:09:56 -0400

    Utilizando PHP en la práctica

--- php_regex.php
+++ php_regex.php
@@ -5,20 +5,31 @@ $file = fopen("./results.csv", "r");
 $match = 0;
 $nomatch = 0;
 
+$t = time();
+
 while(!feof($file)) {
     $line = fgets($file);
     if(preg_match(
-        '/^2018\-01\-(\d\d),.*$/',
+        '/^(\d{4}\-01\-\d\d),(.+),(.+),(\d+),(\d+),.*$/i',
         $line,
         $m
     )
     ) {
-        print_r($m);
-        $match++;  
-    } else {
-        $nomatch++;
+        if($m[4] == $m[5]){
+            printf("Empate: ");
+        }elseif($m[4] > $m[5]){
+            echo "local:   ";
+        } else {
+        echo "visitante";
+    }
+    printf("\t%s, %s [%d-%d]\n", $m[2], $m[3], $m[4], $m[5]);
+    $match++;
+} else {
+    $nomatch++;
     }
 }
 fclose($file);
 
 printf("\n\nmatch: %d\nno match: %d\n", $match, $nomatch);
+
+printf("tiempo: %d segundos\n", time() - $t);
