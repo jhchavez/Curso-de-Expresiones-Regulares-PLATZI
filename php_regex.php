d8061ce8924504aaf6da37bc07554de7720816fe
Jhon Chavez <jhchg7@gmail.com>
2020-05-23 23:24:02 -0400

    E.R PHP

---
+++ php_regex.php
@@ -0,0 +1,24 @@
+<?php
+
+$file = fopen("./results.csv", "r");
+
+$match = 0;
+$nomatch = 0;
+
+while(!feof($file)) {
+    $line = fgets($file);
+    if(preg_match(
+        '/^2018\-01\-(\d\d),.*$/',
+        $line,
+        $m
+    )
+    ) {
+        print_r($m);
+        $match++;  
+    } else {
+        $nomatch++;
+    }
+}
+fclose($file);
+
+printf("\n\nmatch: %d\nno match: %d\n", $match, $nomatch);
